cca0d86881357037ab84848873849318f7811e2a
Love Wikberg <love.wick93@gmail.com>
2017-03-09 17:23:18 +0100

    Start up

---
+++ Guldrullen/Guldrullen/Models/Entities/GuldrullenContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Guldrullen.Models.Entities
+{
+    public partial class GuldrullenContext : DbContext
+    {
+        public virtual DbSet<Movie> Movie { get; set; }
+        public virtual DbSet<Review> Review { get; set; }
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Guldrullen;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        
+    }
+}
---
+++ Guldrullen/Guldrullen/Models/GuldrullenContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Guldrullen.Models.Entities
+{
+    public partial class GuldrullenContext : DbContext
+    {
+        public GuldrullenContext(DbContextOptions<GuldrullenContext> options) : base(options)
+        {
+
+        }
+    }
+}
