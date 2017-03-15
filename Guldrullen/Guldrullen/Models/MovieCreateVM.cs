22ac1351b58bb16fe6ceb8a112679cde98ac4997
Love Wikberg <love.wick93@gmail.com>
2017-03-15 14:38:26 +0100

    kalle

--- Guldrullen/Guldrullen/Models/MovieCreateVM.cs
+++ Guldrullen/Guldrullen/Models/MovieCreateVM.cs
@@ -9,6 +9,7 @@ namespace Guldrullen.Models
     public class MovieCreateVM
     {
         [Display(Name = "Title")]
+        [MaxLength(50, ErrorMessage = "Only 50 character allowed")]
         [Required(ErrorMessage = "*Required")]
         public string Title{ get; set; }
 
@@ -17,6 +18,7 @@ namespace Guldrullen.Models
         public int Length{ get; set; }
 
         [Display(Name = "Genre")]
+        [MaxLength(25, ErrorMessage = "Only 25 character allowed")]
         [Required(ErrorMessage = "*Required")]
         public string Genre{ get; set; }
     }
